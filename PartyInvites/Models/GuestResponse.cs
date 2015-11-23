4d0abc3c8ad326e911852d9f71143e8f121a04cb
ajhudson <andyhudson1979@gmail.com>
2015-11-23 14:22:58 +0000

    Added data annotations.

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required]
         public string Name { get; set; }
     }
 }
